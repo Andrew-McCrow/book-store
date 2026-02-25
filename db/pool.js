c5d93b4be1581c95d93f8691195084dc885f9eed
Andrew-McCrow <andrewmccrow@gmail.com>
2026-02-25 10:56:14 +0000

    updated pool.js file so that db connection is in Railway rather than local host

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,24 @@
+// CODE FOR LOCAL POSTGRESQL DATABASE CONNECTION
+// require("dotenv").config();
+// const { Pool } = require("pg");
+
+// module.exports = new Pool({
+//   host: process.env.DB_HOST || "localhost",
+//   user: process.env.DB_USER,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+//   port: process.env.DB_PORT || 5432,
+// });
+
+// CODE FOR RAILWAY POSTGRESQL DATABASE CONNECTION
 require("dotenv").config();
 const { Pool } = require("pg");
 
-module.exports = new Pool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432,
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+module.exports = pool;
